ae58b90f7b2c895e18141a3bf3d7382ad8eb74e8
Halsey <connerhalsey03@gmail.com>
2025-07-23 22:31:06 -0400

    Working version for deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Farmhouse Mercantile</h1>
+      <p>Welcome to your React-powered restaurant app!</p>
+    </div>
+  );
+}
+
+export default App;
