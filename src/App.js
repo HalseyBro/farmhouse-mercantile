59fe2a9ecf282ec081324187f7335363d7362808
Halsey <connerhalsey03@gmail.com>
2025-07-23 22:03:42 -0400

    Fix: react-scripts added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Farmhouse Mercantile</h1>
+      <p>Your modern restaurant app is under construction.</p>
+    </div>
+  );
+}
+
+export default App;
